8a50e85aae2873e043bc1ed2df3dca63d9e9a719
Erwan <erwan.guichard97421@gmail.com>
2020-03-18 12:43:34 +0400

    Rectangular transposition

---
+++ app/src/main/java/com/fr/codage/RectTranspoActivity.java
@@ -0,0 +1,14 @@
+package com.fr.codage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RectTranspoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rect_transpo);
+    }
+}
