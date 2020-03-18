0e11a5f41b71c7ef0a77d143a518e976cabc2a0d
Erwan <erwan.guichard97421@gmail.com>
2020-03-18 12:25:53 +0400

    César

---
+++ app/src/main/java/com/fr/codage/CesarActivity.java
@@ -0,0 +1,14 @@
+package com.fr.codage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CesarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cesar);
+    }
+}
