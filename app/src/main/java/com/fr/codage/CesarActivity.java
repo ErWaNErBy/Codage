6261ab6bf6fdfc8490c96daca37541f618c0d584
Erwan <erwan.guichard97421@gmail.com>
2020-03-27 22:12:27 +0400

    César

--- app/src/main/java/com/fr/codage/CesarActivity.java
+++ app/src/main/java/com/fr/codage/CesarActivity.java
@@ -1,13 +1,10 @@
 package com.fr.codage;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,14 +36,21 @@ public class CesarActivity extends AppCompatActivity {
             "0x207f", "0x00b2", "0x25a0", "0x00a0" };
 
     // Liste des décimaux des caractères étendus
-    public static final int[] DECIMALS = { 199, 252, 233, 226, 228, 224, 229, 231, 234, 235, 232, 239, 238,
-            236, 196, 197, 201, 230, 198, 244, 246, 242, 251, 249, 255, 214, 220, 162, 163, 165, 8359,
-            402, 225, 237, 243, 250, 241, 209, 170, 186, 191, 8976, 172, 189, 188, 161, 171, 187, 9617,
-            9618, 9619, 9474, 9508, 9569, 9570, 9558, 9557, 9571, 9553, 9559, 9565, 9564, 9563, 9488,
-            9492, 9524, 9516, 9500, 9472, 9532, 9566, 9567, 9562, 9556, 9577, 9574, 9568, 9552, 9580,
-            9575, 9576, 9572, 9573, 9561, 9560, 9554, 9555, 9579, 9578, 9496, 9484, 9608, 9604, 9612,
-            9616, 9600, 945, 223, 915, 960, 931, 963, 181, 964, 934, 920, 937, 948, 8734, 966, 949,
-            8745, 8801, 177, 8805, 8804, 8992, 8993, 247, 8776, 176, 8729, 183, 8730, 8319, 178, 9632, 160 };
+    public static final int[] DECIMALS = {     199,   252,   233,   226,
+            228,   224,   229,   231,   234,   235,   232,   239,   238,
+            236,   196,   197,   201,   230,   198,   244,   246,   242,
+            251,   249,   255,   214,   220,   162,   163,   165,  8359,
+            402,   225,   237,   243,   250,   241,   209,   170,   186,
+            191,  8976,   172,   189,   188,   161,   171,   187,  9617,
+            9618, 9619,  9474,  9508,  9569,  9570,  9558,  9557,  9571,
+            9553, 9559,  9565,  9564,  9563,  9488,  9492,  9524,  9516,
+            9500, 9472,  9532,  9566,  9567,  9562,  9556,  9577,  9574,
+            9568, 9552,  9580,  9575,  9576,  9572,  9573,  9561,  9560,
+            9554, 9555,  9579,  9578,  9496,  9484,  9608,  9604,  9612,
+            9616, 9600,   945,   223,   915,   960,   931,   963,   181,
+             964,  934,   920,   937,   948,  8734,   966,   949,  8745,
+            8801,  177,  8805,  8804,  8992,  8993,   247,  8776,   176,
+            8729,  183,  8730,  8319,   178,  9632,   160 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,7 +111,7 @@ public class CesarActivity extends AppCompatActivity {
     // Fonction qui crypte ou décrypte avec la méthode de cryptage César et qui prend en paramètre un message à crypter/décrypter et le décalage de caractère
     public void cesar(String mess, int key){
 
-        StringBuilder messEncrypt = new StringBuilder("");	// Message résultat qui contiendra le cryptage
+        StringBuilder messEncrypt = new StringBuilder();	// Message résultat qui contiendra le cryptage
 
         for (int i = 0; i < mess.length(); i++) {   // Analyse chaque caractère du message saisi
 
@@ -119,10 +123,9 @@ public class CesarActivity extends AppCompatActivity {
                 String i1 = String.valueOf(mess.charAt(i+1));
                 String i2 = String.valueOf(mess.charAt(i+2));
                 if(i0.equals("\\") && i1.equals("0") && i2.equals("x")) {   // Si les 3 valeurs à partir de i du message contient bien : \0x   ..
-                        String hexaCode = String.valueOf(mess.charAt(i+3)) + String.valueOf(mess.charAt(i+4)) + String.valueOf(mess.charAt(i+5)) + String.valueOf(mess.charAt(i+6)); // On récuperer les 4 caractères hexa après le \0x
-                        int codePointOfHexaCode = Integer.parseInt(hexaCode,16);    // On le converti l'hexa en décimal
-                        codePoint = codePointOfHexaCode;    // On change de décimal avec la nouvelle valeur
-                        i=i+6;  // On passe au caractère suivant
+                    String hexaCode = String.format("%s%s%s%s", mess.charAt(i+3), mess.charAt(i+4), mess.charAt(i+5), mess.charAt(i+6)); // On récuperer les 4 caractères hexa après le \0x
+                    codePoint = Integer.parseInt(hexaCode,16);    // On change le décimal avec la nouvelle valeur (on converti l'hexa en décimal)
+                    i=i+6;  // On passe au caractère suivant
                 }
             }
             // ]
@@ -152,7 +155,7 @@ public class CesarActivity extends AppCompatActivity {
                 }
             } else {                                                    // Sinon (c'est un caractère de la table ASCII normal) ..
                 String normalCharacter = Character.toString( (char) (codePointDecal) );                                 // On récupère le caractère décalé avec son décimal
-                String hexaOfNormalChar = "\\"+ Integer.toHexString( codePointDecal | 0x10000).substring(1);         // On récupère l'hexa du caractère décalé ( de la forme \0x**** )
+                String hexaOfNormalChar = "\\0x"+ Integer.toHexString( codePointDecal | 0x10000).substring(1);         // On récupère l'hexa du caractère décalé ( de la forme \0x**** )
                 messEncrypt.append(normalCharacter.replaceAll("\\p{C}", "\\"+hexaOfNormalChar) );    //  On ajoute dans le message résultat le caracère décalé de la table ASCII normal et si il n'est pas affichable on ajoute à la place son hexa
             }
         }
@@ -171,7 +174,7 @@ public class CesarActivity extends AppCompatActivity {
     }
     // Fonction qui récupère la valeur hexa  et retourne le caractère qui lui correspond (par exemple avec hexa + décalage de 0 : 00e9 -> é )
     private static String hexToAscii(String hexStr) {
-        StringBuilder output = new StringBuilder("");
+        StringBuilder output = new StringBuilder();
         for (int i = 0; i < hexStr.length(); i += 2) {
             String str = hexStr.substring(i, i + 2);
             output.append((char) Integer.parseInt(str, 16));
